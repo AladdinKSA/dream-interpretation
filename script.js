ce65f630e1488dc3149e780c9895fa632cc3c8db
AladdinKSA <mishaalham88@gmail.com>
2025-02-14 19:15:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const corsOptions = {
   loadingIndicator.style.display = 'block';
   
     try {
-    /*  const response = await fetch('https://api.openai.com/v1/chat/completions', {*/
+   
           const response = await fetch('https://api.openai.com/v1', {
         method: 'POST',
         headers: {
